d42d0f53997343a217390df6e18763d34c626efd
Kelly Finnegan <finnegak@email.sc.edu>
2023-06-27 23:36:40 -0400

    Added Prework Files to repo

---
+++ IOS_prework/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  IOS_prework
+//
+//  Created by Kelly Finnegan on 6/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgrounColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    func changeColor() -> UIColor{
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
+}
+
---
+++ IOS_preworkUITests/IOS_preworkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IOS_preworkUITestsLaunchTests.swift
+//  IOS_preworkUITests
+//
+//  Created by Kelly Finnegan on 6/27/23.
+//
+
+import XCTest
+
+final class IOS_preworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
